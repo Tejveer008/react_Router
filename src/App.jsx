4924e464ea44a4707b489a4fb5863ef751cb8c33
Tejveer008 <tejveer.sf@gmail.com>
2024-12-29 11:45:21 +0530

    commit

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { About } from "./pages/About";
+import { Contact } from "./pages/Contact";
+import { Movies } from "./pages/Movies";
+import { AppLayout } from "./components/layout/AppLayout";
+
+export const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/about",
+          element: <About />,
+        },
+        {
+          path: "/contact",
+          element: <Contact />,
+        },
+        { 
+          path: "/movies", element: <Movies /> 
+        },
+      ]
+    },
+   
+
+  ]);
+  // const router = createBrowserRouter(
+
+  //   createRoutesFromElements(
+  //     <>
+  //     <Route path="/home" element={<Home/>}/>
+  //     <Route path="/about" element={<About/>}/>
+  //     <Route path="/movies" element={<Movies/>}/>
+  //     <Route path="/contact" element={<Contact/>}/>
+  //     </>
+  //   )
+  // );
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,6 @@
+
+export const Contact = () => {
+  return (
+    <h1>Contact Page</h1>
+  )
+};
